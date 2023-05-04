23b35c715ff823bd896f47fb36a5f3840feab534
sergueison <serguei.son@gmail.com>
2023-05-04 15:11:50 -0400

    Reformat to remove too many blank lines

--- vacuum_world.py
+++ vacuum_world.py
@@ -1,23 +1,23 @@
 class Environment:
 
-    def __init__(self, dirtyA, dirtyB, location):
-        self.dirtyA = dirtyA
-        self.dirtyB = dirtyB
+    def __init__(self, dirty_a, dirty_b, location):
+        self.dirty_a = dirty_a
+        self.dirty_b = dirty_b
         self.location = location
         self.history = []
 
     def update(self, action):
-        self.history.append((self.dirtyA, self.dirtyB, action))
+        self.history.append((self.dirty_a, self.dirty_b, action))
         if self.location == 'A':
             if action == 'Right':
                 self.location = 'B'
             elif action == 'Suck':
-                self.dirtyA &= False
+                self.dirty_a &= False
         if self.location == 'B':
             if action == 'Left':
                 self.location = 'A'
             elif action == 'Suck':
-                self.dirtyB &= False
+                self.dirty_b &= False
 
     def performance(self):
         len = 0
@@ -29,7 +29,7 @@ class Environment:
 
 
 def simple_reflex_agent(state):
-    if state.location == 'A' and state.dirtyA or state.location == 'B' and state.dirtyB:
+    if state.location == 'A' and state.dirty_a or state.location == 'B' and state.dirty_b:
         return 'Suck'
     elif state.location == 'A':
         return 'Right'
@@ -37,10 +37,10 @@ def simple_reflex_agent(state):
 
 
 def main():
-    for dirtyA in [True, False]:
-        for dirtyB in [True, False]:
+    for dirty_a in [True, False]:
+        for dirty_b in [True, False]:
             for location in ['A', 'B']:
-                env = Environment(dirtyA, dirtyB, location)
+                env = Environment(dirty_a, dirty_b, location)
                 for i in range(3):
                     env.update(simple_reflex_agent(env))
                 print(env.performance())
